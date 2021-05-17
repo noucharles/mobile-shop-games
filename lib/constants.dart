c5aa845709735541e080e93ab0b257753d57ee54
noucharles <noucharles@gmail.com>
2021-05-17 17:25:49 +0100

    traduction fr constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app_flutter/size_config.dart';
 
-const String appName = "E-Shopee";
+const String appName = "237 Gaming";
 
 const kPrimaryColor = Color(0xFFFF7643);
 const kPrimaryLightColor = Color(0xFFFFECDF);
