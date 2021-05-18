39407f51b75be4d9fa86e1573ab7a13a80e0e4fd
noucharles <noucharles@gmail.com>
2021-05-18 23:35:38 +0100

    Retouche product card

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -99,7 +99,7 @@ class ProductCard extends StatelessWidget {
                       flex: 5,
                       child: Text.rich(
                         TextSpan(
-                          text: "${product.discountPrice} FCFA\n",
+                          text: "${product.discountPrice} F\n",
                           style: TextStyle(
                             color: kPrimaryColor,
                             fontWeight: FontWeight.w700,
@@ -107,7 +107,7 @@ class ProductCard extends StatelessWidget {
                           ),
                           children: [
                             TextSpan(
-                              text: "\₹${product.originalPrice}",
+                              text: "${product.originalPrice} F",
                               style: TextStyle(
                                 color: kTextColor,
                                 decoration: TextDecoration.lineThrough,
